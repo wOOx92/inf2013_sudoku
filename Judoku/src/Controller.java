929d69280da69b6cd00c4ba10a4cde357cc8eaba
Flo <i13029@hb.dhbw-stuttgart.de>
2014-06-20 10:18:32 +0200

    InitGui

--- Judoku/src/Controller.java
+++ Judoku/src/Controller.java
@@ -1,11 +1,13 @@
 
 public class Controller {
 //Commit Test
-	
-	
 	public static void main (String[]args){
 		
 		
 		//Test 
+	}
+	
+	public static void initGui(){
+		System.out.println("hallo");
 	}
 }
