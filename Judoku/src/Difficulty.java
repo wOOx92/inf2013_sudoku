9898c342d0b019d9f634cffecabf9015c2552299
DH10STF <DH10STF@DH10STF-DM.homag.com>
2014-06-20 11:07:46 +0200

    Classes

---
+++ Judoku/src/Difficulty.java
@@ -0,0 +1,23 @@
+//Alternative
+//Index zwischen 1 und 100 als Schwierigkeit
+//In der Oberfläche direkt angeben oder per Umrechnung -> Easy = Random zwischen 1 und 20
+
+public enum Difficulty {
+	EASY,
+	MEDIUM,
+	HARD,
+	EXTREME;
+	
+	public int toInt(){
+		if(this == EASY){
+			return 0;
+		}
+		if(this == MEDIUM){
+			return 1;
+		}
+		if(this == HARD){
+			return 2;
+		}
+		return 3;
+	}
+}
