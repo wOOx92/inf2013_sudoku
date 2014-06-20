c61998d4f33192453d96720c3bcd3d400a778d08
DH10STF <DH10STF@DH10STF-DM.homag.com>
2014-06-20 11:39:43 +0200

    Grundstruktur

--- Judoku/src/Gui.java
+++ Judoku/src/Gui.java
@@ -1,7 +1,35 @@
+//JTable 9*9
+//Buttons
+	//Hint
+	//GenerateNew
+	//Reset
+	//Difficulty auswahl
+//Events für jeden Button
+
+//Ändert die Gui Table direkt das Objekt?
 
 public class Gui {
+	private INumberPuzzle gameField;
 	
+	//Wird aufgerufen wenn das gameField geändert wird oder resettet wird.
+	public void refreshView(){
+		
+	}
 	
+	public void setGameField(INumberPuzzle newGameField){
+		this.gameField = newGameField;
+		refreshView();
+	}
 	
-
+	public void resetGame(){
+		this.gameField.reset();
+		refreshView();
+	}
+	
+	//Button clicked events für jeden Button
+	
+	//GridChangedEvent
+	
+	//Buttons und JTable
 }
+
