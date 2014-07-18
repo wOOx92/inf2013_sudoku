9fb3ba058bacd53cfd805cbc60dba626913295d6
DH10JAM <DH10JAM@DH10JAM-DM.homag.com>
2014-07-18 09:06:18 +0200

    Created Class

--- Judoku/src/dhbw/project/judokugame/WindowHelp.java
+++ Judoku/src/dhbw/project/judokugame/WindowHelp.java
@@ -9,8 +9,10 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.MenuSelectionManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.StyleConstants;
 import javax.swing.JButton;
 
 import dhbw.project.judokugame.GUI_Window.JudokuButtonListener;
@@ -27,7 +29,7 @@ public class WindowHelp extends JFrame {
     private JButton btnSudokuRules;
     private JButton btnFunctions;
 	private JButton btnCredits;
-	private JTextArea txtCenterText;
+	private JTextPane txtCenterText;
 	private JPanel pnlTextArea;
 	private JPanel pnlButtons;
 	/**
@@ -40,31 +42,38 @@ public class WindowHelp extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(5, 5));
 		setContentPane(contentPane);
-		setSize(500, 300);
+		setSize(600, 300);
 		setResizable(false);
 		
-		JPanel pnlButtons = new JPanel();
+		pnlButtons = new JPanel();
 		pnlButtons.setLayout(new GridLayout(4,1,10,10));
 		
 		contentPane.add(pnlButtons, BorderLayout.WEST);
-		JButton btnInfo = new JButton("Info");
+		btnInfo = new JButton("Info");
+		btnInfo.setContentAreaFilled(false);
 		btnInfo.addActionListener(new WindowHelpButtonListener());
-		JButton btnSudokuRules = new JButton("Sudoku Rules");
+		btnSudokuRules = new JButton("Sudoku Rules");
 		btnSudokuRules.addActionListener(new WindowHelpButtonListener());
-		JButton btnFunctions = new JButton("Functions");
+		btnSudokuRules.setContentAreaFilled(false);
+		btnFunctions = new JButton("Functions");
 		btnFunctions.addActionListener(new WindowHelpButtonListener());
-		JButton btnCredits = new JButton("Credits");
+		btnFunctions.setContentAreaFilled(false);
+		btnCredits = new JButton("Credits");
 		btnCredits.addActionListener(new WindowHelpButtonListener());
+		btnCredits.setContentAreaFilled(false);
 		pnlButtons.add(btnInfo);
 		pnlButtons.add(btnSudokuRules);
 		pnlButtons.add(btnFunctions);
 		pnlButtons.add(btnCredits);
-	
-		JPanel pnlTextArea = new JPanel();
-		contentPane.add(pnlTextArea, BorderLayout.CENTER);
-		JTextArea txtCenterText = new JTextArea(100, 100);
 		
-		pnlTextArea.add(txtCenterText);
+		
+		txtCenterText = new JTextPane();
+		txtCenterText.setContentType("text/html");
+		txtCenterText.setBackground(contentPane.getBackground());
+		contentPane.add(txtCenterText, BorderLayout.CENTER);
+		
+		
+		
 		
 		
 	}
@@ -73,20 +82,45 @@ public class WindowHelp extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource() == btnInfo) {
-			txtCenterText.setText("Here comes the Info");
-			pnlTextArea.repaint();
+			txtCenterText.setText("<html><font face = 'arial'>This game is a programming-project.</font face></html>");
+		
 			} 
 			else if (e.getSource() == btnSudokuRules) {
-				txtCenterText.setText("Rulez");
-				pnlTextArea.repaint();
+				txtCenterText.setText("<html><font face = 'arial'>Completed puzzles are always a type of Latin square with an additional constraint on the contents of individual regions. \n"
+						+ "For example, the same single integer may not appear twice in the same 9×9 playing board row or column or in any of the nine 3×3 \n"
+						+ "subregions of the 9×9 playing board.</font face></html>");
+			
 				} 
 			    else if (e.getSource() == btnFunctions) {
-			    	txtCenterText.setText("Functionz");
-			    	pnlTextArea.repaint();
+			    	txtCenterText.setText("<html> <font face = 'arial'>"
+			    			+ "<b>New Game</b>: You can chose between tree different levels (easy/medium/hard). By selecting one of this levels, a new sudoku puzzle will be created.\n"
+			    			+ "<br>"
+			    			+ "<b>Hint Button</b>: Gives a hint. If there exist a false integer it will be underlined in red. If all integers are correct, a new one will appear.\n"
+			    			+ "<br>"
+			    			+ "<b>Undo Button</b>: The last integer will dissapear, but you can push this button only 5times in a row.\n"
+			    			+ "<br>"
+			    			+ "<b>Redo Button</b>: Recreates the last integer which was deleted by the undo function. "
+			    			+ "<br>"
+			    			+ "<b>Validate Button</b>: Validates the Puzzle. An info will appear if there are mistakes left. If there are no mistakes left the game ends\n"
+			    			+ "<br>"
+			    			+ "<b>Help</b>: Opens the window you see right now\n"
+			    			+ "<br>"
+			    			+ "<b>Reset</b>: Resets the sudoku-gamefield. A new game will be created.\n"
+			    			+ "<br>"
+			    			+ "<b>Exit</b>: Closes the Game.</font face></html>");
+			    	
 					} 
 			    	else if (e.getSource() == btnCredits) {
-			    		txtCenterText.setText("Credits");
-			    		pnlTextArea.repaint();
+			    		txtCenterText.setText("<html><font face = 'arial'><b>This game is proudly presented by:</b>"
+			    				+ "<p></p>"
+			    				+ "Florian Steurer"
+			    				+ "<br>"
+			    				+ "Markus Wingler"
+			    				+ "<br>"
+			    				+ "Dennis Uteg"
+			    				+ "<br>"
+			    				+ "Michael Jauch</font face></html>");
+			    	
 						} 
 			
 		}
