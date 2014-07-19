410a2d786660f1f2f54aa9b7fd2fa5ff4c677514
DH10STF <DH10STF@DH10STF-DM.homag.com>
2014-07-19 18:46:20 +0200

    fixed package name in package info

--- Judoku/JUnitTestSrc/dhbw/project/judoku/junitest/package-info.java
+++ Judoku/JUnitTestSrc/dhbw/project/judoku/junitest/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package provides JUnit Tests for the most crucial methods and classes from dhbw.procect.judokugame
+ * This package provides JUnit Tests for the most crucial methods and classes from dhbw.project.judokugame
  */
 /**
  * @author Dennis Uteg, Florian Steurer, Markus Wingler, Michael Jauch
