aa9f47f52d3862e86f9dadfedeeb2ffbb0e451cc
dh10stf <dh10stf@DH10STF-DM.homag.com>
2014-08-05 11:39:18 +0200

    ,,

--- Judoku/JUnitTestSrc/dhbw/project/judoku/junitest/ControllerTest.java
+++ Judoku/JUnitTestSrc/dhbw/project/judoku/junitest/ControllerTest.java
@@ -7,22 +7,20 @@ import org.junit.Test;
 import dhbw.project.judokugame.Controller;
 import dhbw.project.puzzlemodel.Sudoku;
 
-public class ControllerTest {
+public class ControllerTest extends Controller{
 
 	@Test
 	public void trySetValueTest() {
-		Controller c = new Controller();
 		Sudoku dummy = new Sudoku(3);
 		
-		
-		assertFalse("Strings should not be accepted.", c.trySetValue(1, 3, "a", dummy));
-		assertFalse("Strings should not be accepted.", c.trySetValue(1, 3, "string", dummy));
-		assertFalse("A too big value has been accepted.", c.trySetValue(1, 3, "10", dummy));
-		assertTrue("Valid request was denied.", c.trySetValue(1, 3, "0", dummy));
-		assertTrue("Valid request was denied.", c.trySetValue(1, 3, "9", dummy));
-		assertFalse("Invalid x-Value accepted.", c.trySetValue(10, 8, "4", dummy));
-		assertFalse("Invalid y-Value accepted.", c.trySetValue(5, 11, "4", dummy));
-		assertFalse("Negative x-Value accepted.", c.trySetValue(-4, 8, "4", dummy));
-		assertFalse("Negative y-Value accepted.", c.trySetValue(4, -4, "4", dummy));
+		assertFalse("Strings should not be accepted.", this.trySetValue(1, 3, "a", dummy));
+		assertFalse("Strings should not be accepted.", this.trySetValue(1, 3, "string", dummy));
+		assertFalse("A too big value has been accepted.", this.trySetValue(1, 3, "10", dummy));
+		assertTrue("Valid request was denied.", this.trySetValue(1, 3, "0", dummy));
+		assertTrue("Valid request was denied.", this.trySetValue(1, 3, "9", dummy));
+		assertFalse("Invalid x-Value accepted.", this.trySetValue(10, 8, "4", dummy));
+		assertFalse("Invalid y-Value accepted.", this.trySetValue(5, 11, "4", dummy));
+		assertFalse("Negative x-Value accepted.", this.trySetValue(-4, 8, "4", dummy));
+		assertFalse("Negative y-Value accepted.", this.trySetValue(4, -4, "4", dummy));
 	}
 }
